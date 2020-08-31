dda9a0b22e205bd2f44a43866f438aa47ed8b64b
freedomchurl <freedomchurl@naver.com>
2020-08-31 13:25:10 +0900

    Save

--- FrontSide/edu-platform/src/components/LectureList.vue
+++ FrontSide/edu-platform/src/components/LectureList.vue
@@ -1,41 +1,73 @@
 <template>
   <div id="main-text">
-    <h1 id = "main-title">수강신청</h1>
-<hr id = "main-line">
-<input id = "search" type="text" name="search" value="검색어를 입력하세요!">
-<button id="search-button" type="button" name="name">찾기</button>
+    <h1 id="main-title">수강신청</h1>
+    <hr id="main-line" />
+    <div class="row upper-box">
+      <div class="col-sm-10 col-sm-offset-5">
+        <label class="sr-only" for="adminid">Admin ID</label>
+        <input
+          v-bind:class="textinput"
+          type="text"
+          id="adminid"
+          v-model="loginID"
+          title="managerid"
+          placeholder="강의명을 입력해 주세요"
+        />
+      </div>
 
-  <div id = "lecture-list">
-    <div>
-      <img src="classImage.PNG" alt="My Image">
-      <p>Class Title</p>
+      <button v-bind:class="primebtn" @click="modifyNotice">검색</button>
+    </div>
+    <!-- <button id="search-button" type="button" name="name">찾기</button> -->
+
+    <hr id="below-search" />
+
+    <div id="lecture-list">
+      <div>
+        <img :src="sampleImg" alt="My Image" />
+        <p>Class Title</p>
+      </div>
+
+      <div>
+        <img :src="sampleImg" alt="My Image" />
+        <p>Class Title</p>
+      </div>
+
+      <div>
+        <img :src="sampleImg" alt="My Image" />
+        <p>Class Title</p>
+      </div>
+
+      <div>
+        <img :src="sampleImg" alt="My Image" />
+        <p>Class Title</p>
+      </div>
+
+      <div>
+        <img :src="sampleImg" alt="My Image" />
+        <p>Class Title</p>
+      </div>
+
+      <div>
+        <img :src="sampleImg" alt="My Image" />
+        <p>Class Title</p>
+      </div>
     </div>
 
-    <div>
-      <img src="classImage.PNG" alt="My Image">
-      <p>Class Title</p>
+    <div class="container">
+			<div class="row">
+				<div class="col">
+					<ul class="pagination justify-content-center">
+						<li class="page-item"><a class="page-link" href="#">Previous</a></li>
+						<li class="page-item active"><a class="page-link" href="#">1</a></li>
+						<li class="page-item"><a class="page-link" href="#">2</a></li>
+						<li class="page-item"><a class="page-link" href="#">3</a></li>
+						<li class="page-item"><a class="page-link" href="#">4</a></li>
+						<li class="page-item"><a class="page-link" href="#">5</a></li>
+						<li class="page-item"><a class="page-link" href="#">Next</a></li>
+					</ul>
+				</div>
+			</div>
     </div>
-
-    <div>
-      <img src="classImage.PNG" alt="My Image">
-      <p>Class Title</p>
-    </div>
-
-    <div>
-      <img src="classImage.PNG" alt="My Image">
-      <p>Class Title</p>
-    </div>
-
-    <div>
-      <img src="classImage.PNG" alt="My Image">
-      <p>Class Title</p>
-    </div>
-
-    <div>
-      <img src="classImage.PNG" alt="My Image">
-      <p>Class Title</p>
-    </div>
-</div>
   </div>
 </template>
 
@@ -44,8 +76,9 @@ export default {
   data() {
     return {
       textinput: "form-control",
+      sampleImg: require("../../static/img/classImage.png"),
       primebtn:
-        "row-sm-1 col-sm-1.5 btn btn-primary disabled btn-sm below-button",
+        "row-sm-1 col-lg-1 btn btn-primary disabled btn-sm below-button",
     };
   },
   methods: {
@@ -57,6 +90,16 @@ export default {
 </script>
 
 <style scoped>
+#below-search {
+}
+
+.upper-box {
+  display: flex;
+  /* height: 50px; */
+  align-items: center;
+  justify-content: space-around;
+}
+
 #main-text {
   margin: 5rem 20% 2rem 20%;
 }
@@ -69,16 +112,14 @@ export default {
   font-weight: bold;
   margin-left: 5%;
 }
- #page-title {
-  }
 
-  #page-title-line {
-    height : 5px;
-    background : rgb(25,94,76);
-  }
-
-  #lecture-list {
-    display : flex;
-  }
-
+#lecture-list {
+  margin: 2rem 0rem 5rem 0rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.btn-primary {
+  background-color: rgb(94, 94, 94);
+}
 </style>
