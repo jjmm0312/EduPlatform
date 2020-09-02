b834fa95ec5e7b1207918b76c08d7db4dfd861e1
jjmm0312 <198032@naver.com>
2020-09-03 05:44:35 +0900

    file upload / download

--- BackEnd/eduplatform/src/main/java/vaninside/eduplatform/controller/FileController.java
+++ BackEnd/eduplatform/src/main/java/vaninside/eduplatform/controller/FileController.java
@@ -1,9 +1,16 @@
 package vaninside.eduplatform.controller;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -30,27 +37,16 @@ public class FileController {
 		return fileService.fileUpload(files);	
 	}
 
-	public ResponseEntity<Resource> download() {
-		 // Load file as Resource
-        Resource resource = service.loadFileAsResource(fileName);
- 
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
-        }
- 
-        // Fallback to the default content type if type could not be determined
-        if(contentType == null) {
-            contentType = "application/octet-stream";
-        }
- 
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
-    }
+	@ApiOperation(value = "파일 다운로드", notes = "파일을 다운로드한다.")
+	@RequestMapping( "/download" )
+	public ResponseEntity<Resource> download(@RequestParam String filepath) throws IOException {
+		Path path = Paths.get(filepath);
+		String contentType = Files.probeContentType(path);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+
+		Resource resource = new InputStreamResource(Files.newInputStream(path));
+		return new ResponseEntity<>(resource, headers, HttpStatus.OK);
 	}
 }
