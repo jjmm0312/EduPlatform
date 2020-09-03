ca1fd9bcc869691034bd35fe261b7c872abbf040
jjmm0312 <198032@naver.com>
2020-09-03 22:07:26 +0900

    Add API Connection

--- FrontSide/edu-platform/src/components/SignUpPage.vue
+++ FrontSide/edu-platform/src/components/SignUpPage.vue
@@ -5,7 +5,7 @@
 
     <div class="input-box">
     <span id="type">유형</span>
-    <select name>
+    <select v-model="roletype">
       <option value>학생</option>
       <option value>강사</option>
     </select>
@@ -17,11 +17,11 @@
     <div class="col-sm-5 col-sm-offset-5">
     <input
           v-bind:class="textinput"
-          type="password"
+          type="text"
           id="adminpwd"
-          v-model="loginPass"
+          v-model="name"
           title="managerpass"
-          placeholder="비밀번호"
+          placeholder="이름"
         />
     </div>
 </div>
@@ -31,14 +31,14 @@
     <div class="col-sm-5 col-sm-offset-5">
     <input
           v-bind:class="textinput"
-          type="password"
+          type="text"
           id="adminpwd"
-          v-model="loginPass"
+          v-model="username"
           title="managerpass"
-          placeholder="비밀번호"
+          placeholder="아이디"
         />
     </div>
-<button v-bind:class="primebtn" @click="loginEdu">로그인</button>
+<button v-bind:class="primebtn" :disabled="buttondisable"  @click="idCheck">{{buttonMessage}}</button>
     </div>
     <br />
     <div class="input-box">
@@ -48,7 +48,7 @@
           v-bind:class="textinput"
           type="password"
           id="adminpwd"
-          v-model="loginPass"
+          v-model="pwd"
           title="managerpass"
           placeholder="비밀번호"
         />
@@ -62,9 +62,9 @@
           v-bind:class="textinput"
           type="password"
           id="adminpwd"
-          v-model="loginPass"
+          v-model="pwdcheck"
           title="managerpass"
-          placeholder="비밀번호"
+          placeholder="비밀번호확인"
         />
     </div>
     </div>
@@ -74,25 +74,11 @@
     <div class="col-sm-5 col-sm-offset-5">
     <input
           v-bind:class="textinput"
-          type="password"
+          type="text"
           id="adminpwd"
-          v-model="loginPass"
+          v-model="email"
           title="managerpass"
-          placeholder="비밀번호"
-        />
-    </div>
-    </div>
-    <br />
-    <div class="input-box">
-    <span id="phone">전화번호</span>
-    <div class="col-sm-5 col-sm-offset-5">
-    <input
-          v-bind:class="textinput"
-          type="password"
-          id="adminpwd"
-          v-model="loginPass"
-          title="managerpass"
-          placeholder="비밀번호"
+          placeholder="이메일"
         />
     </div>
     </div>
@@ -102,29 +88,103 @@
     <div class="col-sm-5 col-sm-offset-5">
     <input
           v-bind:class="textinput"
-          type="password"
+          type="text"
           id="adminpwd"
-          v-model="loginPass"
+          v-model="invite"
           title="managerpass"
-          placeholder="비밀번호"
+          placeholder="초대코드"
         />
     </div>
     </div>
     <br />
     <br />
-<button v-bind:class="primebtn" @click="loginEdu">로그인</button>
+<button v-bind:class="primebtn" @click="signup">가입</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import IP from "../../static/IP";
+
 export default {
     data() {
     return {
-      loginID: "",
-      loginPass: "",
+      roletype:"",
+      buttonMessage:"중복 확인",
+      buttondisable:false,
+      name:"",
+      username: "",
+      email:"",
+      pwd:"",
+      pwdcheck:"",
+      invite:"",
       primebtn: "btn btn-primary disabled btn-block",
       textinput: "form-control",
     };
+  },
+  methods: {
+    idCheck() {
+      console.log("ID check");
+      var vm = this;
+      axios
+        .get("http://" + IP.IP + ":8080/auth/check", {
+          params: { username: vm.username },
+          timeout: 10000,
+        })
+        .then((res) => {
+          if (res.data.result == false)
+            alert("아이디가 중복입니다!");
+          else{
+            alert("확인 완료되었습니다");
+            vm.buttonMessage = "확인완료";
+            vm.buttondisable = true;
+          }
+        });
+    },
+    signup() {
+      console.log("Sign Up");
+      var vm = this;
+      var myRole = "학생";
+
+      if(this.roletype == "학생"){
+        myRole = "ROLE_STUDENT";
+      } 
+      else if(this.roletype == "강사") {
+        myRole = "ROLE_TEACHER";
+      }
+
+      if(this.pwd !== this.pwdcheck) {
+        alert("비밀번호를 확인하세요.");
+        return;
+      }
+
+      if(this.buttondisable == false) {
+        alert("아이디 중복확인하세요.");
+        return;
+      } 
+
+      axios
+        .post("http://" + IP.IP + ":8080/auth/signup", {
+          username:vm.username,
+          name:vm.name,
+          email:vm.email,
+          password:vm.pwd,
+          code :vm.invite,
+          role : myRole,
+          timeout: 10000,
+        })
+        .then((res) => {
+          if (res.data.status == 2)
+            alert("아이디가 중복입니다!");
+          else if(res.data.status == 3){
+            alert("초대코드가 잘못되었습니다.")
+          }else
+            {
+            alert("가입 완료되었습니다");
+            console.log("회원가입 페이지 Okay");
+          }
+        }); 
+    },
   },
 };
 </script>
