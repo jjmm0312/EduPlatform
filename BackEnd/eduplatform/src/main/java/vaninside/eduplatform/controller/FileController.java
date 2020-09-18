2c47d28717d4c674bc30999c9e31787f63c28802
jjmm0312 <198032@naver.com>
2020-09-18 17:17:10 +0900

    Edit Readme

--- BackEnd/eduplatform/src/main/java/vaninside/eduplatform/controller/FileController.java
+++ BackEnd/eduplatform/src/main/java/vaninside/eduplatform/controller/FileController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -32,13 +33,13 @@ public class FileController {
 	
 	
 	@ApiOperation(value = "파일 업로드", notes = "파일을 업로드한다.")
-	@RequestMapping( "/upload" )
+	@PostMapping( "/upload" )
 	public List<String> upload(@RequestParam("file") List<MultipartFile> files) throws Exception {
 		return fileService.fileUpload(files);	
 	}
 
 	@ApiOperation(value = "파일 다운로드", notes = "파일을 다운로드한다.")
-	@RequestMapping( "/download" )
+	@PostMapping( "/download" )
 	public ResponseEntity<Resource> download(@RequestParam String filepath) throws IOException {
 		Path path = Paths.get(filepath);
 		String contentType = Files.probeContentType(path);
